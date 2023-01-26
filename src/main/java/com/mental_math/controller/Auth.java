58525f8f521f48383b01eddf3244067a93f58a56
noahmancino <noahdanielmancino@gmail.com>
2023-01-26 13:57:10 -0500

    init

---
+++ src/main/java/com/mental_math/controller/Auth.java
@@ -0,0 +1,7 @@
+package com.mental_math.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Auth {
+}
---
+++ src/test/java/com/example/mental_math/MentalMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mental_math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalMathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
